a2d4beb65ac91d38c4ab08a261e22e46d90326a1
Dristi Shah <dristishah@dristis-air.attlocal.net>
2019-02-25 22:19:18 -0500

    adding initial python

---
+++ sleepBreath.py
@@ -0,0 +1,9 @@
+# write your import statements at the top
+# make sure that you have eveything that you import already installed
+# you will need to use pip3 to install these softwares such as
+# pip3 install [PACKAGE_NAME]
+# the [x] means that you replace that whole thing with whatever package you
+#     want to install
+# you might need to install stuff such as numpy pandas and matplotlib
+# you also need to add this stuff to github and ask me how to do that
+# ASK ME HOW TO UPLOAD YOUR PROJECT TO GITHUB
